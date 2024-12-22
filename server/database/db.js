6c5510673a6654e841f7622a9707f2da911c9d2b
Marvin Eversley Silva <meverss@gmail.com>
2024-12-22 09:53:38 -0500

    Changed DB to MongoDB Atlas

--- server/database/db.js
+++ server/database/db.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
 	try {
-	    await mongoose.connect('mongodb://localhost/squidusers')
+//	    await mongoose.connect('mongodb://localhost/squidusers')
+	    await mongoose.connect('mongodb+srv://meverss:PM6MFwIEnUABjgVR@cluster0.l4evt.mongodb.net/squidusers')
 	    console.log('>>> Connected to Database.')
 	} catch (error) {
-	    colsole.log(error)
+	    console.log(error)
 	}
 } 
