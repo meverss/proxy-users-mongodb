8390e1462ac41852a274bda6527ea6ed2c08c951
Marvin Eversley Silva <meverss@gmail.com>
2024-12-22 13:37:11 -0500

    Update

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
+    const USER = process.env.DB_USER
+    const PASSWORD = process.env.DB_PASSWORD
 	try {
 //	    await mongoose.connect('mongodb://localhost/squidusers')
-	    await mongoose.connect('mongodb+srv://meverss:PM6MFwIEnUABjgVR@cluster0.l4evt.mongodb.net/squidusers')
+	    await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.l4evt.mongodb.net/squidusers`)
 	    console.log('>>> Connected to Database.')
 	} catch (error) {
 	    console.log(error)
